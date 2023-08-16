5dd28388c20334fb7e9050bedd237cb5484b3e31
Nishimura <Nishimura@NIshimura.local>
2023-08-16 15:48:38 +0900

    README作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,52 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.scss';
+import Sidebar from './components/sidebar/Sidebar';
+import Chat from './components/chat/Chat';
+import Login from './components/login/Login';
+import { useAppDispatch, useAppSelector } from './app/hooks';
+import { auth } from './firebase';
+import { login, logout } from './features/userSlice';
+import { ErrorBoundary } from 'react-error-boundary';
+import { ErrorFallBack } from './utils/ErroorFallBack'
 
 function App() {
+  const user = useAppSelector((state) => state.user.user);
+  // const user = null;
+  // console.log(user);
+
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((loginUser) => {
+      console.log(loginUser)
+      if(loginUser) {
+        dispatch(login({
+          uid: loginUser.uid,
+          photo: loginUser.photoURL,
+          email: loginUser.email,
+          displayName: loginUser.displayName,
+        })
+        );
+      } else {
+        dispatch(logout());
+      }
+    })
+  }, [dispatch]);
+
   return (
     <div className="App">
-
+      {user ? (
+      <>
+      <ErrorBoundary FallbackComponent={ ErrorFallBack }>
+       <Sidebar />
+      </ErrorBoundary>
+       <Chat />
+      </>
+    ) : (
+      <>
+        <Login />
+      </>
+      )}
     </div>
   );
 }
